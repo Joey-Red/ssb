c3769f7790b784ac2978572cb03d0a5e8c3f8044
Joey Dalrymple <96956760+Joey-Red@users.noreply.github.com>
2026-08-24 14:56:10 -0500

    style: load exact frame-media styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './festival-layout.css'
 import './visual-overrides.css'
 import './festival-component-fixes.css'
 import './festival-surfaces.css'
+import './frame-media.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
