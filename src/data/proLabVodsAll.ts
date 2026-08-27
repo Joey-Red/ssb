8e9af7efbe0eb9b7852546a2153f1e8ef3264482
Joey Dalrymple <96956760+Joey-Red@users.noreply.github.com>
2026-08-27 14:20:41 -0500

    Apply final Pro Lab VOD resolution batch

--- src/data/proLabVodsAll.ts
+++ src/data/proLabVodsAll.ts
@@ -9,6 +9,7 @@ import { applyProVodLinkResolutionBulk7 } from './proLabVodLinkResolutionsBulk7'
 import { applyProVodLinkResolutionBulk8 } from './proLabVodLinkResolutionsBulk8'
 import { applyProVodLinkResolutionBulk9 } from './proLabVodLinkResolutionsBulk9'
 import { applyProVodLinkResolutionBulk10 } from './proLabVodLinkResolutionsBulk10'
+import { applyProVodLinkResolutionBulk11 } from './proLabVodLinkResolutionsBulk11'
 import { proVodCatalog2026Batch1 } from './proLabVods2026Batch1'
 import { proVodCatalog2026Batch2 } from './proLabVods2026Batch2'
 import { proVodCatalog2026Batch3 } from './proLabVods2026Batch3'
@@ -46,16 +47,18 @@ const unresolvedCatalog = [
 ] as readonly ProVodRecord[]
 
 export const proVodCatalog = unresolvedCatalog.map((vod) =>
-  applyProVodLinkResolutionBulk10(
-    applyProVodLinkResolutionBulk9(
-      applyProVodLinkResolutionBulk8(
-        applyProVodLinkResolutionBulk7(
-          applyProVodLinkResolutionBulk6(
-            applyProVodLinkResolutionBulk5(
-              applyProVodLinkResolutionBulk4(
-                applyProVodLinkResolutionBulk3(
-                  applyProVodLinkResolutionBulk2(
-                    applyProVodLinkResolutionBulk1(applyProVodLinkResolution2026Batch5(vod)),
+  applyProVodLinkResolutionBulk11(
+    applyProVodLinkResolutionBulk10(
+      applyProVodLinkResolutionBulk9(
+        applyProVodLinkResolutionBulk8(
+          applyProVodLinkResolutionBulk7(
+            applyProVodLinkResolutionBulk6(
+              applyProVodLinkResolutionBulk5(
+                applyProVodLinkResolutionBulk4(
+                  applyProVodLinkResolutionBulk3(
+                    applyProVodLinkResolutionBulk2(
+                      applyProVodLinkResolutionBulk1(applyProVodLinkResolution2026Batch5(vod)),
+                    ),
                   ),
                 ),
               ),
