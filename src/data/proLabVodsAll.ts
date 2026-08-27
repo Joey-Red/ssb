34999247ee18500cffdcf4c016a7fe8869244e93
Joey Dalrymple <96956760+Joey-Red@users.noreply.github.com>
2026-08-27 12:47:40 -0500

    Wire tight-anchor Pro Lab VOD recoveries

--- src/data/proLabVodsAll.ts
+++ src/data/proLabVodsAll.ts
@@ -5,6 +5,7 @@ import { applyProVodLinkResolutionBulk3 } from './proLabVodLinkResolutionsBulk3'
 import { applyProVodLinkResolutionBulk4 } from './proLabVodLinkResolutionsBulk4'
 import { applyProVodLinkResolutionBulk5 } from './proLabVodLinkResolutionsBulk5'
 import { applyProVodLinkResolutionBulk6 } from './proLabVodLinkResolutionsBulk6'
+import { applyProVodLinkResolutionBulk7 } from './proLabVodLinkResolutionsBulk7'
 import { proVodCatalog2026Batch1 } from './proLabVods2026Batch1'
 import { proVodCatalog2026Batch2 } from './proLabVods2026Batch2'
 import { proVodCatalog2026Batch3 } from './proLabVods2026Batch3'
@@ -42,12 +43,14 @@ const unresolvedCatalog = [
 ] as readonly ProVodRecord[]
 
 export const proVodCatalog = unresolvedCatalog.map((vod) =>
-  applyProVodLinkResolutionBulk6(
-    applyProVodLinkResolutionBulk5(
-      applyProVodLinkResolutionBulk4(
-        applyProVodLinkResolutionBulk3(
-          applyProVodLinkResolutionBulk2(
-            applyProVodLinkResolutionBulk1(applyProVodLinkResolution2026Batch5(vod)),
+  applyProVodLinkResolutionBulk7(
+    applyProVodLinkResolutionBulk6(
+      applyProVodLinkResolutionBulk5(
+        applyProVodLinkResolutionBulk4(
+          applyProVodLinkResolutionBulk3(
+            applyProVodLinkResolutionBulk2(
+              applyProVodLinkResolutionBulk1(applyProVodLinkResolution2026Batch5(vod)),
+            ),
           ),
         ),
       ),
